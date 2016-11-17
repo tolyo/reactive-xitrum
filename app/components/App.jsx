25d81e5a94859f9f9e8a004f1e9a6fc2f7cf7cd7
tolyo <anatolyostrovsky@gmail.com>
2016-11-17 10:00:00 +0200

    Clean up App component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,43 +1,37 @@
 import React from 'react';
 import 'whatwg-fetch';
 
-let app;
-
 export default class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { quote : {} };
-        app = this;
-    }
+  constructor(props) {
+    super(props);
+      this.state = { quote : {} };
+  }
 
-    update(json) {
-        this.setState({quote : json})
-    }
+  getQuote() {
+    fetch('quote')
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      this.setState({quote : json})
+     });
+  }
 
-    getQuote() {
-        fetch('quote')
-        .then((res) => {
-            return res.json();
-        })
-        .then((json) => {
-            app.update(json);
-        });
-    }
+  componentWillMount() {
+    this.getQuote();
+  }
 
-    componentWillMount() {
-        app.getQuote();
-    }
+  render() {
+    return (
+      <div className="container" id="content">
+        <div className="starter-template">
+          <h1>{this.state.quote.author}</h1>
+          <p>{this.state.quote.content}</p>
+          <button type="button" className="btn btn-primary" onClick={()=>{this.getQuote()}}>Get quote</button>
+        </div>
+      </div>
+    );
+  }
 
-    render() {
-        return (
-            <div className="container" id="content">
-                <div className="starter-template">
-                    <h1>{this.state.quote.author}</h1>
-                    <p>{this.state.quote.content}</p>
-                    <button type="button" className="btn btn-primary" onClick={ this.getQuote }>Get quote</button>
-                </div>
-            </div>
-        );
-    }
 }
