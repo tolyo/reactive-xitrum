9c98c7e2ee7b71f15a246836b87fe79cdf5f99d0
tolyo <anatolyostrovsky@gmail.com>
2016-12-04 23:26:23 +0200

    Add form

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import 'whatwg-fetch';
+import React from "react";
+import "whatwg-fetch";
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { quote : {} };
+    this.state = { quote : {}, author: "", newQuote: "" };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    fetch('quote', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    })
   }
 
   getQuote() {
@@ -36,6 +47,34 @@ export default class App extends React.Component {
             Get quote
           </button>
         </div>
+
+        <hr />
+
+        <form onSubmit={ (e) => this.handleSubmit(e)} className="col-xs-12 col-md-6 col-lg-4">
+
+          <div className="form-group">
+            <label htmlFor="quote">Author: </label>
+            <input className="form-control"
+                   type="text"
+                   id="author"
+                   value={this.state.author}
+                   onChange={ (e) => this.setState({author: e.target.value}) } />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="quote">New quote: </label>
+            <input className="form-control"
+                   type="text"
+                   id="quote"
+                   value={this.state.newQuote}
+                   onChange={ (e) => this.setState({newQuote: e.target.value}) } />
+          </div>
+          <button type="submit"
+                  className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+
       </div>
     );
   }
--- src/main/scala/quickstart/action/SiteIndex.scala
+++ src/main/scala/quickstart/action/SiteIndex.scala
@@ -1,9 +1,21 @@
 package quickstart.action
 
-import xitrum.{Action, Log}
-import xitrum.annotation.GET
+import io.netty.handler.codec.http.HttpResponseStatus
+import xitrum.annotation.{GET, POST}
+import xitrum.{Action, Log, SkipCsrfCheck}
 
 import scala.util.Random
+
+object Data {
+  var quoteList = List(
+    Quote("Larry Wall", "The is more than one method to our madness"),
+
+    Quote("Chuck Norris", "I only use the Roundhouse kick"),
+
+    Quote("Eric Raymond","Being a social outcast helps you stay concentrated on the really important things, like thinking and hacking")
+  )
+
+}
 
 @GET("")
 class SiteIndex extends Action with Log {
@@ -17,16 +29,19 @@ class SiteIndex extends Action with Log {
 @GET("/quote")
 class QuoteAction extends Action with Log {
 
-  val quoteList = List(
-    Quote("Larry Wall", "The is more than one method to our madness"),
-
-    Quote("Chuck Norris", "I only use the Roundhouse kick"),
-
-    Quote("Eric Raymond","Being a social outcast helps you stay concentrated on the really important things, like thinking and hacking")
-  )
-
   def execute() {
-    respondJson(quoteList(Random.nextInt(quoteList.size)))
+    respondJson(Data.quoteList(Random.nextInt(Data.quoteList.size)))
+  }
+}
+
+@POST("/quote")
+class SaveQuote extends Action with Log with SkipCsrfCheck {
+
+  def execute() = {
+    log.info(params("author").head)
+    log.info(params("newQuote").head)
+    Data.quoteList = Quote(params("author").head, params("newQuote").head) :: Data.quoteList
+    response.setStatus(HttpResponseStatus.OK)
   }
 }
 
